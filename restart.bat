70763d43ec0351eef4c3a25fad0b5a50c26fcb40
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2017-10-25 14:26:58 +0800

    Update restart.bat

--- restart.bat
+++ restart.bat
@@ -1,1 +1,7 @@
-bash -c "while true; do sleep 1s; if redis-cli get shutdown | grep -q 1; then echo shutdown; exit; else echo not; fi; done"
+bash -c "while true; do sleep 1s; if redis-cli get shutdown | grep -q 1; then redis-cli set shutdown 0; echo shutdown; exit; else echo not; fi; done"
+
+redis-cli psetex shutdown 3000 1
+
+
+
+
