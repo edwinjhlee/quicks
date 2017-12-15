04003b0c662713ff508a7e92dc3a49a9a9c5765b
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2017-12-15 23:44:43 +0800

    Create latex.sh

---
+++ docs/installation/latex.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+apt-get install texlive-base
+apt-get install biblatex
+apt-get install texlive-fonts-recommended
+
+apt-get install texlive-fonts-extra
+apt-get install texlive-science
