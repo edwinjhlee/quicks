e9f22bdd277e99be7eea759fc863d4a929da9095
Edwin.JH.Lee <edwinjhlee@users.noreply.github.com>
2017-12-15 10:38:05 +0800

    Create mongodb-3-6.xenial64.sh

---
+++ docs/installation/mongodb-3-6.xenial64.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+
+apt-get update
+apt-get install -y mongodb-org
